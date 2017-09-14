703836b459df08c2c76bfb5aa0ec5b6fa4c9c167
josh <pgmjsd@gmail.com>
2017-09-14 17:47:45 -0400

    added linear and exponential backoff

--- retry/retry.go
+++ retry/retry.go
@@ -22,81 +22,111 @@ type State struct {
 	Reason         StopReason
 	Result         interface{}
 	Err            error
+	lastBackoff    time.Duration
+}
+
+type BackoffStrategy interface {
+	computeBackoff(state *State) time.Duration
+}
+
+type LinearBackoff struct {
+	delay	time.Duration
+}
+
+func (l *LinearBackoff) computeBackoff(state *State) time.Duration {
+	return l.delay // Same delay every time.
+}
+
+type ExponentialBackoff struct {
+	initialDelay	time.Duration
+	factor			float32
+}
+
+func ComputeExponentialBackoff(initial time.Duration, last time.Duration, factor float32) time.Duration {
+	if last == 0 {
+		return initial
+	}
+	nanos := float32(last.Nanoseconds()) * factor
+	return time.Duration(int64(nanos))
+}
+
+func (e *ExponentialBackoff) computeBackoff(state *State) time.Duration {
+	return ComputeExponentialBackoff(e.initialDelay, state.lastBackoff, e.factor)
 }
 
 type Config struct {
 	MaxAttempts int
 	MaxDuration time.Duration        // Retry time limit.
 	RetryError  func(err error) bool // Returns true if function can be retried.
-	// backoffConfig SomeBackoffInterfaceUnionThing?
+	Backoff		BackoffStrategy		 // Linear or exponential backoff.
 }
 
 func ShouldRetry(err error) bool {
 	return err != nil
 }
 
-func (r *Config) Execute(f Function) (interface{}, error, *State) {
-	if r.RetryError == nil {
-		r.RetryError = ShouldRetry
+func (c *Config) Execute(f Function) (interface{}, error, *State) {
+	if c.RetryError == nil {
+		c.RetryError = ShouldRetry
 	}
 
-	ctx := State{
+	if c.Backoff == nil {
+		c.Backoff = &LinearBackoff{ delay: time.Duration(100 * time.Millisecond)}
+	}
+
+	state := State {
 		StartTimestamp: time.Now(),
 		Attempts:       0,
 		Err:            nil,
 		Reason:         NonRetryableError,
 	}
 
-	for ctx.Attempts = 0; true ; ctx.Attempts++ {
-		rv, e := f(&ctx)	// Call the function.
+	for state.Attempts = 0; true ; state.Attempts++ {
+		rv, e := f(&state) // Call the function.
 		if e != nil {
-			ctx.Err = e
+			state.Err = e
 		}
 
-		if !r.RetryError(e) {	// Exit if there is an error or other condition.
-			ctx.Result = rv
-			return rv, e, &ctx
+		if !c.RetryError(e) { // Exit if there is an error or other condition.
+			state.Result = rv
+			if e == nil {
+				state.Reason = Success
+			}
+			return rv, e, &state
 		}
 
-		backoffDuration := r.computeBackoff(&ctx)
-		if backoffDuration.Nanoseconds() > 10 {
-			time.Sleep(backoffDuration)
-		}
+		delay := c.Backoff.computeBackoff(&state)
+		time.Sleep(delay)
+		state.lastBackoff = delay
 	}
 	panic("How did you get here?")
 }
 
-func (r *Config) computeBackoff(ctx *State) time.Duration {
-	// TODO: Fixed and exponential backoff.
-	return time.Duration(100 * time.Millisecond)
-	// exponential =
-}
-
-func (r *Config) shouldRetry(ctx *State, err error) bool {
-	if !r.RetryError(ctx.Err) {
-		ctx.stop(NonRetryableError, "")
+func (c *Config) shouldRetry(state *State, err error) bool {
+	if !c.RetryError(state.Err) {
+		state.stop(NonRetryableError, "")
 		return false
 	}
 
-	if ctx.Attempts > r.MaxAttempts {
-		ctx.stop(MaxAttemptsExceeded, "max attempts exceeded")
+	if state.Attempts > c.MaxAttempts {
+		state.stop(MaxAttemptsExceeded, "max attempts exceeded")
 		return false
 	}
 
 	now := time.Now()
-	elapsed := now.Sub(ctx.StartTimestamp)
+	elapsed := now.Sub(state.StartTimestamp)
 
-	if elapsed.Nanoseconds() > r.MaxDuration.Nanoseconds() {
-		ctx.stop(MaxDurationExceeded, "max duration exceeded")
+	if elapsed.Nanoseconds() > c.MaxDuration.Nanoseconds() {
+		state.stop(MaxDurationExceeded, "max duration exceeded")
 		return false
 	}
 
 	return true
 }
 
-func (ctx *State) stop(reason StopReason, msg string) {
-	if ctx.Err == nil && msg != "" {
-		ctx.Err = errors.New(msg)
+func (s *State) stop(reason StopReason, msg string) {
+	if s.Err == nil && msg != "" {
+		s.Err = errors.New(msg)
 	}
-	ctx.Reason = reason
+	s.Reason = reason
 }
--- retry/retry_test.go
+++ retry/retry_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"errors"
 	"fmt"
+	"time"
 )
 
 type Foo struct {
@@ -12,8 +13,8 @@ type Foo struct {
 }
 
 func TestFunctionCall(t *testing.T) {
-	fn := func(ctx *State) (val interface{}, err error) {
-		if ctx.Attempts > 3 {
+	fn := func(state *State) (val interface{}, err error) {
+		if state.Attempts > 3 {
 			return &Foo { 42, "blort" }, nil
 		}
 		e := errors.New("blort")
@@ -22,14 +23,32 @@ func TestFunctionCall(t *testing.T) {
 
 	r := Config{
 		MaxAttempts: 10,
+		Backoff: &ExponentialBackoff{
+			initialDelay: time.Duration(100 * time.Millisecond),
+			factor: 2.0,
+			},
 	}
 
-	rv, e, ctx := r.Execute(fn)
+	rv, e, state := r.Execute(fn)
+
+	fmt.Printf("rv=%v state=%v\n", rv, state)
 
 	if e != nil {
-		return
-		}
+		t.Error("Expected nil error!")
+	}
 
+	if state.Reason != Success {
+		t.Error("Expected Success!")
+	}
+}
 
-	fmt.Printf("rv=%v ctx=%v\n", rv, ctx)
+func TestCalculateExponentialBackoff(t *testing.T) {
+	initial := time.Duration(5)
+	var last time.Duration
+	for i := 0; i < 10 ; i++ {
+		last = ComputeExponentialBackoff(initial, last, 2.0)
+	}
+	if last.Nanoseconds() != 2560 {
+		t.Errorf("Expected 2560, got %v", last)
+	}
 }
